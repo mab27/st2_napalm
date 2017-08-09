a49f2d925c5d206f96cc8ef776da1bd240393981
mab27 <mehdi_abdelouahab@hotmail.com>
2017-08-09 16:30:30 +0200

    minor changes

--- my_pack/actions/render_cfg.py
+++ my_pack/actions/render_cfg.py
@@ -3,15 +3,15 @@ from st2actions.runners.pythonrunner import Action
 
 
 class render_cfg(Action):
-    def run(self, driver, router_Id, local_ASN, remote_ASN, peer_IP):
+    def run(self, driver, router_Id, local_ASN, remote_ASN, peer_IP, remote_hostname):
 
-        path_to_template_file = "/opt/stackstorm/packs/default/actions/templates/template_" + driver + ".j2"
-        path_to_rendered_file = "/home/mab/automation/st2_napalm/rendered_files/cfg_" + driver +"_" + router_Id + "_.txt"
+        path_to_template_file = "/opt/stackstorm/packs/default/actions/template_files/template_ebgp_" + driver + ".j2"
+        path_to_rendered_file = "/opt/stackstorm/packs/default/actions/rendered_files/cfg_ebgp_" + driver +"_" + router_Id + "_.txt"
 
         template_file=open(path_to_template_file)
         s=template_file.read()
         template=Template(s)
-        rendering = template.render(router_Id=router_Id, local_ASN=local_ASN, remote_ASN=remote_ASN, peer_IP=peer_IP)
+        rendering = template.render(router_Id=router_Id, local_ASN=local_ASN, remote_ASN=remote_ASN, peer_IP=peer_IP, remote_hostname=remote_hostname)
 
         file=open(path_to_rendered_file,"w")
         file.write(rendering)
